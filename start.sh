d4d911323456d95fa6dbcfbc0231a27411c3f4d4
Kris Uz <60334543+KrisUz@users.noreply.github.com>
2021-05-11 02:02:59 -0600

    Update requirements
    add twilio

--- start.sh
+++ start.sh
@@ -4,18 +4,18 @@ VIRTUALENV=.venv
 if [ ! -d $ROOT_ENV ]; then
   python3 -m venv $ROOT_ENV
 fi
-if [ ! -f $ROOT_ENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $ROOT_ENV/bin/python
-  $ROOT_ENV/bin/pip install virtualenv
+if [ ! -f $ROOT_ENV/Scripts/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $ROOT_ENV/Scripts/python
+  $ROOT_ENV/Scripts/pip install virtualenv
 fi
 if [ ! -d $VIRTUALENV ]; then
-    $ROOT_ENV/bin/virtualenv $VIRTUALENV
+    $ROOT_ENV/Scripts/virtualenv $VIRTUALENV
 fi
 if [ ! -f .env ]; then
     cp .env.example .env
     refresh
 fi
-source $VIRTUALENV/bin/activate
+source $VIRTUALENV/Scripts/activate
 echo "Activated virtualenv Python is `which python`"
 pip install -r requirements.txt
 python app.py 
